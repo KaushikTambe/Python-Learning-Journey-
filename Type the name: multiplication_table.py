84a3ff409d3470d83f87fbf5efaf31c113d9b97d
Kaushik Suryakant tambe <kaushiktambe1999@gmail.com>
2026-05-26 14:57:04 +0530

    Change date format in multiplication_table.py
    Updated the date format in the multiplication table script.

--- Type the name: multiplication_table.py
+++ Type the name: multiplication_table.py
@@ -1,6 +1,6 @@
 # Multiplication Table Generator
 # Learned: for loops, f-strings, user input
-# Date: May 2026
+# Date: 26-05-2026
 
 Table = int(input("Which table do you want? "))
 for i in range(1,11):
