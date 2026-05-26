5025f513e664ab6fdb78b23ab398ed0ddab77f56
Kaushik Suryakant tambe <kaushiktambe1999@gmail.com>
2026-05-26 14:55:45 +0530

    Changed the Table with table

--- Type the name: multiplication_table.py
+++ Type the name: multiplication_table.py
@@ -2,6 +2,6 @@
 # Learned: for loops, f-strings, user input
 # Date: May 2026
 
-table = int(input("Which table do you want? "))
-for i in range(1, 11):
-    print(f"{table} x {i} = {table * i}")
+Table = int(input("Which table do you want? "))
+for i in range(1,11):
+    print(f"{Table} x {i} = {Table * i}")
